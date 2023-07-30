e543a9f3d44f36f5bffd39d9383026f5692a99bc
josevictormoreno <josevictor.admoreno@gmail.com>
2023-07-30 17:39:22 -0300

    Implementação do stateful widget - AULA 13

--- my_app/lib/home_page.dart
+++ my_app/lib/home_page.dart
@@ -1,26 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/home_controller.dart';
 
-class HomePage extends StatelessWidget {
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var controller = HomeController.of(context);
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Nav bar'),
+//       ),
+//       body: Center(
+//         child: Text(
+//           'Numero: ${controller.value}',
+//           style: const TextStyle(fontSize: 35),
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: const Icon(Icons.add),
+//         onPressed: () {
+//           controller.increment();
+//         },
+//       ),
+//     );
+//   }
+// }
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+
+  @override
   Widget build(BuildContext context) {
-    var controller = HomeController.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Nav bar'),
       ),
       body: Center(
         child: Text(
-          'Numero: ${controller.value}',
+          'Numero: ${counter}',
           style: const TextStyle(fontSize: 35),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          controller.increment();
+          setState(() {
+            counter++;
+          });
         },
       ),
     );
