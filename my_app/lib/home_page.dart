b84ca1fa3d43827de89ac8bd1e678d881654a8ed
josevictormoreno <josevictor.admoreno@gmail.com>
2023-07-31 20:33:57 -0300

    Aula 17 list view

--- my_app/lib/home_page.dart
+++ my_app/lib/home_page.dart
@@ -8,7 +8,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var counter = 0;
+  final names = [
+    'aninha',
+    'tais',
+    'talita',
+    'robs',
+    'zevs',
+    'valen',
+    'lucao',
+    'mara',
+    'uilson'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,18 +27,14 @@ class _HomePageState extends State<HomePage> {
           title: const Text('Nav bar'),
         ),
         body: Center(
-          child: SingleChildScrollView(
-              child: Column(
-            children: [
-              for (var i = 0; i < 10; i++)
-                Container(
-                  width: double.infinity,
-                  height: 80,
-                  margin: const EdgeInsets.all(5),
-                  color: Colors.blue,
-                )
-            ],
-          )),
+          child: ListView.builder(
+              itemCount: 9,
+              itemBuilder: (context, index) {
+                final name = names[index];
+                return ListTile(
+                  title: Text('${name}'),
+                );
+              }),
         ));
   }
 }
